e691a96c254b986d078d9b8686fcaa315f94a817
Fam. Ott <Fam. Ott>
2016-08-04 18:17:17 +0200

    Moved to new folder and added controller

---
+++ controller/LogManager.php
@@ -0,0 +1,15 @@
+<?php
+namespace controller;
+
+require_once("model/LogCollection.php");
+require_once("model/LogItem.php");
+
+require_once("view/LogView.php");
+
+class LogManager {
+
+    public function __construct() {
+
+    }
+
+}
